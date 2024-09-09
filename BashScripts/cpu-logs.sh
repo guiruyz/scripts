d0afd60d1cab5d9d4a8d01238b3b2aa879c7511c
guiruyz <guiruyz@gmail.com>
2024-09-09 10:47:52 -0300

    add instructions

--- BashScripts/cpu-logs.sh
+++ BashScripts/cpu-logs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Log file location
-LOGFILE="/path/to/cpu_usage_per_core.log"
+# verify if there's an argument
+if [ "$#" -ne 1 ]; then
+    echo "Uso: $0 <arquivo_log>"
+    exit 1
+fi
 
-# Create the log file if it doesn't exist
+# informations
+LOG_FILE=$1
+
 touch "$LOGFILE"
 
-# Infinite loop to log CPU usage every second
+# send commands and add datas to logs
 while true
 do
-    # Get the current timestamp
+
     timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
-    # Get the CPU usage per core using mpstat
     echo "Timestamp: $timestamp" >> "$LOGFILE"
     
     mpstat -P ALL 1 1 | awk -v timestamp="$timestamp" '
@@ -24,6 +28,5 @@ do
         printf "%s CPU%s Usage: %.2f%%\n", timestamp, data["CPU"], usage
     }' >> "$LOGFILE"
 
-    # Sleep for 1 second before the next iteration
     sleep 1
 done
