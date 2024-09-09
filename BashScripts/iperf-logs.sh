96d309ccf702a21a1ce2dcc3c33e983a3d788fea
guiruyz <guiruyz@gmail.com>
2024-09-09 13:32:17 -0300

    added bw in iperf parameters

--- BashScripts/iperf-logs.sh
+++ BashScripts/iperf-logs.sh
@@ -2,20 +2,21 @@
 
 # verify if there's an argument
 if [ "$#" -ne 3 ]; then
-    echo "Uso: $0 <servidor_ip> <porta> <arquivo_log>"
+    echo "Uso: $0 <ip> <porta> <banda> <arquivo_log>"
     exit 1
 fi
 
 # informations sent by user
 SERVER_IP=$1
 PORT=$2
-LOG_FILE=$3
+BANDA=$3
+LOG_FILE=$54
 
 # descriptions 
 echo "Capturando saída do iperf do servidor $SERVER_IP na porta $PORT..."
 echo "A saída será registrada em: $LOG_FILE"
 
 # commands
-iperf -c "$SERVER_IP" -p "$PORT" | while read line; do
+iperf -c "$SERVER_IP" -u -i 1 -l 1300 -p "$PORT" -t 999 -b "$BANDA"M | while read line; do
     echo "$(date '+%Y-%m-%d %H:%M:%S') $line" | tee -a "$LOG_FILE"
 done
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### There's some files into this repositories, with log capture proposes
 #### start iperf-logs
 ```
-./iperf-logs.sh <ip> <porta> <arquivo_log>
+./iperf-logs.sh <ip> <porta> <banda> <arquivo_log>
 ```
 #### cpu-logs
 ##### capture only one cpu
