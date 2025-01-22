f69bc272d5fa42ca4341eed58324302aa79045f2
guiruyz <guiruyz@gmail.com>
2025-01-22 16:30:57 -0300

    Added CPU power consumption

--- powerMeasure/plot-power-measured.py
+++ powerMeasure/plot-power-measured.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv_file = "power_log.csv"
+csv_file = "power_log_2025-01-22_18-54-48.csv"
 
 data = pd.read_csv(csv_file)
 
 data['timestamp'] = pd.to_datetime(data['timestamp'])
 
 plt.figure(figsize=(10, 6))
-plt.plot(data['timestamp'], data['current_reading'], label='Consumo Atual (Watts)', marker='o')
+plt.plot(data['timestamp'], data['realtime_power'], label='Consumo Atual (Watts)', marker='o')
+plt.plot(data['timestamp'], data['cpu_power'], label='Consumo Atual de CPU (Watts)', marker='o')
 
 plt.title('Consumo de Energia do Servidor')
 plt.xlabel('Timestamp')
--- powerMeasure/power-measure.sh
+++ powerMeasure/power-measure.sh
@@ -5,26 +5,31 @@ IDRAC_HOST="192.168.10.100"
 IDRAC_USER="root"
 IDRAC_PASS="senha"
 
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
 LOG_DIR="/var/log/idrac"
-LOG_FILE="$LOG_DIR/power_log.csv"
+LOG_FILE="$LOG_DIR/power_log_$NOW.csv"
 
 INTERVAL=10
 
 mkdir -p $LOG_DIR
 
 if [ ! -f "$LOG_FILE" ]; then
-    echo "timestamp,realtime_power,max_last_day,min_last_day" > $LOG_FILE
+    echo "timestamp,realtime_power,cpu_power" > $LOG_FILE
 fi
 
 while true; do
     # Capturando dados do consumo de energia
     OUTPUT=$(sshpass -p "$IDRAC_PASS" ssh -o StrictHostKeyChecking=no $IDRAC_USER@$IDRAC_HOST racadm get system.Power)
+    OUTPUT1=$(sudo timeout -s SIGINT 1s /local/do/cpu-energy-meter/cpu-energy-meter -r)
 
-    REALTIME_POWER=$(echo "$OUTPUT" | grep "#Realtime.Power" | awk -F= '{print $2}' | awk '{print $1}')
-    MAX_LAST_DAY=$(echo "$OUTPUT" | grep "#Max.LastDay=" | awk -F= '{print $2}' | awk '{print $1}')
-    MIN_LAST_DAY=$(echo "$OUTPUT" | grep "#Min.LastDay=" | awk -F= '{print $2}' | awk '{print $1}')
+    CPU0_j=$(echo "$OUTPU1" | grep 'cpu0_package_joules' | awk -F= '{print $2}')
+    ts=$(echo "$OUTPUT1" | grep 'duration_seconds' | awk -F= '{print $2}')
+    CPU_REALTIME_POWER=$(echo "scale=6; $CPU0_j / $ts" | bc -l)
+    TOTAL_REALTIME_POWER=$(echo "$OUTPUT" | grep "#Realtime.Power" | awk -F= '{print $2}' | awk '{print $1}')
+    #MAX_LAST_DAY=$(echo "$OUTPUT" | grep "#Max.LastDay=" | awk -F= '{print $2}' | awk '{print $1}')
+    #MIN_LAST_DAY=$(echo "$OUTPUT" | grep "#Min.LastDay=" | awk -F= '{print $2}' | awk '{print $1}')
 
-    echo "$(date +'%Y-%m-%d %H:%M:%S'),$REALTIME_POWER,$MAX_LAST_DAY,$MIN_LAST_DAY" >> $LOG_FILE
+    echo "$(date +'%Y-%m-%d %H:%M:%S'),$TOTAL_REALTIME_POWER,$CPU_REALTIME_POWER" >> $LOG_FILE
 
     sleep $INTERVAL
 done
